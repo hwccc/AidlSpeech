220b3168c4df7663bec4475db08d6d8a51dcc457
hwccc <342786709@qq.com>
2020-11-28 17:24:36 +0800

    1、增加识别错误状态类

---
+++ aidlspeech/src/main/java/com/hwc/speech/constant/SpeechErrorConstant.java
@@ -0,0 +1,13 @@
+package com.hwc.speech.constant;
+
+/**
+ * @author: hwc
+ * date:   On 2020/11/28
+ */
+public interface SpeechErrorConstant {
+
+    /**
+     * 第三方占用麦克风错误码
+     */
+    String VOICE_START_ERROR_OTHER_USE = "100001";
+}
